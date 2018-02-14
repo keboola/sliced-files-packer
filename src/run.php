ddfb64568464d40db7383bb2d54cb27c20cd355e
Vladimír Kriška <vlado@keboola.com>
2018-02-14 18:56:17 +0100

    Convert errors to exceptions

--- src/run.php
+++ src/run.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__) . "/../vendor/autoload.php");
+require_once(dirname(__FILE__) . "/common.php");
 
 $arguments = getopt("d::", array("data::"));
 if (!isset($arguments["data"])) {
